cfae5026dd6282e66ece904a134b31506ccf56fe
shashank14 <shashankragireddy@Shashanks-MacBook-Pro.local>
2022-12-25 17:02:38 +0530

    new func

--- index.py
+++ index.py
@@ -4,3 +4,11 @@ def new_name():
   print("sdasda")
   print("sdasda")
   print("sdasda")
+
+
+def new_name3():
+  print("sdasda")
+  print("sdasda")
+  print("sdasda")
+  print("sdasda")
+  print("sdasda")
